b9fe86b0e66ea6c296f1cb086ace3256120df293
BruckyZ <bruckzewge1gmail.com>
2017-11-08 17:07:44 -0500

    Initial commit

---
+++ src/main/java/com/example/spingboot_10/Spingboot10Application.java
@@ -0,0 +1,12 @@
+package com.example.spingboot_10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spingboot10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spingboot10Application.class, args);
+	}
+}
