5d4c0a2812807be477e35569c6df82e3b53dbfed
BruckyZ <bruckzewge1gmail.com>
2017-11-09 10:43:15 -0500

    updated

--- src/main/java/com/example/spingboot_10/Course.java
+++ src/main/java/com/example/spingboot_10/Course.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -23,11 +24,11 @@ public class Course
 	private String instructor;
 
 	@NotNull
-	@Size(min=10)
+	@Size(min=10,message="enter min vale")
 	private String description;
 
 	@NotNull
-	@Size(min=3)
+	@Min(3)
 	private int credit;
 
 	public long getId()
